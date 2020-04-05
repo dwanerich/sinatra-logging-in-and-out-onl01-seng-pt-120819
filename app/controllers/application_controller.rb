5841c7a050a61e5f0647a62cc7ce9a0e461a79ba
dwane richards <dwanerich@gmail.com>
2020-04-05 21:22:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username])
-    if @user && @user.password == params[:password]
+    if @user = User.find_by(username: params[:username])
+    # if @user && @user.password == params[:password]
       
       session[:user_id] = @user.id
       
